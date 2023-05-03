132b20ca4dfd9519418e24da945d7697192d0124
israelviana <rael232is@gmail.com>
2023-05-03 00:00:43 -0300

    tela inicial construcao

---
+++ lib/componentes/icon_button_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class IconButtomBar extends StatelessWidget {
+  final String text;
+  final Icon icon;
+  final bool? selected;
+  final VoidCallback? onPressed;
+
+  const IconButtomBar(
+      {Key? key,
+        required this.text,
+        required this.icon,
+        this.selected,
+        this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 20,
+      child: Column(
+        children: [
+          Expanded(
+            child: IconButton(
+                constraints: const BoxConstraints(),
+                padding: EdgeInsets.zero,
+                onPressed: onPressed,
+                icon: icon),
+          ),
+          Text(text, style: TextStyle(fontSize: 10),)
+        ],
+      ),
+    );
+  }
+}
--- lib/views/Home/home_page.dart
+++ lib/views/Home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:happy_place/Services/auth_service.dart';
 import 'package:provider/provider.dart';
-
+import '../../componentes/icon_button_bar.dart';
 import '../../repository/google_sign_in.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,6 +16,62 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 60,
+          padding: const EdgeInsets.only(top: 10, bottom: 8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Expanded(
+                child: InkWell(
+                  onTap: () {},
+                  child: const IconButtomBar(
+                    text: "Meditação",
+                    icon: const Icon(Icons.sensor_occupied_sharp),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () {},
+                  child: const IconButtomBar(
+                    text: "Palavra do dia",
+                    icon: const Icon(Icons.book),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () {},
+                  child: const IconButtomBar(
+                    text: "Home",
+                    icon: const Icon(Icons.house),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () {},
+                  child: const IconButtomBar(
+                    text: "Humor diário",
+                    icon: const Icon(Icons.data_exploration),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () {},
+                  child: const IconButtomBar(
+                    text: "Músicas",
+                    icon: const Icon(Icons.library_music_sharp),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Text("Happy place"),
       ),
